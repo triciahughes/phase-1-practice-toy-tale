b3c762e045041336f9dbc7cae5ab0258dadd8d3e
triciahughes <hughes.a.tricia@gmail.com>
2023-01-18 09:06:49 -0800

    fetched-data

--- src/index.js
+++ src/index.js
@@ -12,4 +12,11 @@ document.addEventListener("DOMContentLoaded", () => {
       toyFormContainer.style.display = "none";
     }
   });
+  fetch("http://localhost:3000/toys")
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
 });
